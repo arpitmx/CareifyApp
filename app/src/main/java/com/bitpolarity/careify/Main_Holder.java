7132cab43f0ffe8899fdd26af7bc84c69bb85e8c
Asus <arpitmaurya01codechef@gmail.com>
2021-05-29 15:20:05 +0530

    Added bottom navigation bars Addded Toolbars

---
+++ app/src/main/java/com/bitpolarity/careify/Main_Holder.java
@@ -0,0 +1,24 @@
+package com.bitpolarity.careify;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.ActionBar;
+import android.view.View;
+import android.os.Bundle;
+
+public class Main_Holder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_holder);
+
+//        this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+//        getSupportActionBar().setDisplayShowCustomEnabled(true);
+//        getSupportActionBar().setCustomView(R.layout.custom_action_bar);
+//        //getSupportActionBar().setElevation(0);
+//        View view = getSupportActionBar().getCustomView();
+
+
+        }
+
+    }
--- app/src/main/java/com/bitpolarity/careify/Registration_Step2.java
+++ app/src/main/java/com/bitpolarity/careify/Registration_Step2.java
@@ -1,19 +1,8 @@
 package com.bitpolarity.careify;
-import com.bitpolarity.careify.R;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class Registration_Step2
@@ -27,7 +16,7 @@ public class Registration_Step2
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration_step2);
 
-        startActivity(new Intent(Registration_Step2.this, HomeActivity.class));
+        startActivity(new Intent(Registration_Step2.this, Main_Holder.class));
 
     }
 
